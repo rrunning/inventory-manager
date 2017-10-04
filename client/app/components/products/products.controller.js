e292e52a52502f08cc185950e2e7c60113667239
rrunning <ryanrunning@securitymetrics.com>
2017-10-03 20:39:36 -0600

    adds images and product names

--- client/app/components/products/products.controller.js
+++ client/app/components/products/products.controller.js
@@ -7,10 +7,20 @@ class ProductsController {
 		this.productDescription = '1';
 		this.productAmount = '';
 		this.productPrice = '';
+		this.carObj = {};
+		this.cheeseObj = {};
 	}
 	fetchProducts() {
 		this.ProductsService.fetchProducts().then(response => {
 			this.productList = response.data;
+			this.carObj = response.data[0];
+			this.cheeseObj = response.data[1];
+			this.carObj.name = 'Ford GT';
+			this.carObj.cost = '$500,000';
+			this.carObj.imgThumbnail = 'http://st.motortrend.com/uploads/sites/5/2016/12/2017-Ford-GT-side-profile.jpg';
+			this.cheeseObj.name = 'Cottage Cheese';
+			this.cheeseObj.cost = '$3.49';
+			this.cheeseObj.imgThumbnail = 'http://www.cheesemaking.com/resize?po=http%3a%2f%2fwww.cheesemaking.com%2fimages%2frecipes%2f85drycurdcottagecheese%2fPics%2fpic04.jpg&bw=1000&w=1000&bh=1000&h=1000';
 		});
 	}
 }
