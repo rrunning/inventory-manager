6b89cedab8a5168008921ff0ab9bc303053270af
rrunning <ryanrunning@securitymetrics.com>
2017-11-16 18:18:14 -0700

    product id adds to the service

--- client/app/components/services/history.js
+++ client/app/components/services/history.js
@@ -7,7 +7,7 @@ let historyModule = angular.module('about.history', [])
 function History($http) {
 	const service = {
 		fetchHistory: () => {
-			return $http.get('http://wta-inventorybackend.herokuapp.com/api/v1/product/summary');
+			return $http.get('http://wta-inventorybackend.herokuapp.com/api/v1/product/:prodId/transactions');
 		},
 	}
 	return service;
