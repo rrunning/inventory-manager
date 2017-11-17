9367edf330b2c297174a70f258fc6d62146be1e6
rrunning <ryanrunning@securitymetrics.com>
2017-11-16 19:31:54 -0700

    finally adds stuff that I kept missing. Cause I didn't do git add

---
+++ client/app/components/services/shoppingCart.js
@@ -0,0 +1,17 @@
+import angular from 'angular';
+
+let shoppingCartModule = angular.module('about.shoppingCart', [])
+	.factory('ShoppingCart', ShoppingCart)
+	.name;
+
+function ShoppingCart($http) {
+	const service = {
+		test: () => {
+			console.log('IT WORKS, ADD SOME STUFF BRO');
+		},
+	}
+	return service;
+}
+
+ShoppingCart.$inject = ['$http'];
+export default shoppingCartModule;
