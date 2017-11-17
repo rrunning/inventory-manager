cc4f7b473d309f2819c28a96e5b86f5bf2f4b8b7
rrunning <ryanrunning@securitymetrics.com>
2017-11-16 19:30:54 -0700

    connects shoppingCart service correctly

--- client/app/components/services/index.js
+++ client/app/components/services/index.js
@@ -2,11 +2,13 @@ import angular from 'angular';
 import ProductsService from './productsService';
 import Authentication from './authentication';
 import History from './history';
+import ShoppingCart from './shoppingCart';
 
 let commonModule = angular.module('about.services', [
 	ProductsService,
 	Authentication,
 	History,
+	ShoppingCart,
 ])
 	.name;
 
