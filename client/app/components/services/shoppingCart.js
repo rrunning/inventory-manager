0966c5d7d56a29452d4375f9fadc421adb5558a7
rrunning <ryanrunning@securitymetrics.com>
2017-12-05 20:53:10 -0700

    cart adds correctly when no product is in it. still needs to add quantity to the product details page so it can populate in the cart

--- client/app/components/services/shoppingCart.js
+++ client/app/components/services/shoppingCart.js
@@ -6,17 +6,40 @@ let shoppingCartModule = angular.module('about.shoppingCart', [])
 
 function ShoppingCart($http) {
 	const service = {
-		cart: {},
+		cart: [
+		],
+		transaction: {
+			type: {
+				id: null,
+				description: null,
+			},
+			date: 'date',
+			note: 'note string',
+			altersId: 'number id of any transaction that this one alters',
+			subTransactions: [
+				{
+					id: 'productId',
+					qty: 'number'
+				}
+			],
+		},
 		test: () => {
 			console.log('IT WORKS, ADD SOME STUFF BRO');
 		},
-		addProduct: (product, qty) => {
-			if (service.cart[product.id]) {
-				service.cart[product.id].qty += qty;
+		addProduct: (product, quantity) => {
+			if (service.cart.length >= 1) {
+				for (i = 0; i < service.cart.length; i++) {
+					if (product.id === [i].id) {
+						[i].qty = [i].qty + quantity
+					}
+				}
 			} else {
-				service.cart[product.id] = { product, qty };
-				console.log(service.cart);
+				service.cart.push({
+					id: product.id,
+					qty: qty,
+				});
 			}
+			console.log(service.cart);
 		}
 	}
 	return service;
