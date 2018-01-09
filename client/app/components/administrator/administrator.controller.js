7e904c9087f5be476294c29dc16788c7b561dd02
rrunning <ryanrunning@securitymetrics.com>
2018-01-08 20:44:01 -0700

    creates form for new product creation. Doesn't quite connect to the API yet

--- client/app/components/administrator/administrator.controller.js
+++ client/app/components/administrator/administrator.controller.js
@@ -2,11 +2,16 @@ class AdministratorController {
 	constructor($stateParams, ProductsService) {
 		this.name = 'admin';
 		this.ProductsService = ProductsService;
+		this.addNew = false;
+		this.newProduct = {};
 	}
 	fetch() {
 		this.ProductsService.fetchProducts();
 	}
 	addNewProduct() {
+		this.addNew = true;
+	}
+	submitNewProduct() {
 		this.ProductsService.createProduct();
 	}
 }
