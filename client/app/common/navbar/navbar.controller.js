3480ed9630b70676c5ae5e35b919b9718bf4a098
rrunning <ryanrunning@securitymetrics.com>
2018-01-09 20:07:09 -0700

    little bit of navbar styling and adds user roles to the navbar. Administrator and dashboard page links aren't visible to non-admins

--- client/app/common/navbar/navbar.controller.js
+++ client/app/common/navbar/navbar.controller.js
@@ -3,11 +3,18 @@ class NavbarController {
     this.name = 'navbar';
     this.Authentication = Authentication;
     this.username = '';
+    this.isAdmin = false;
     this.setUserAs();
     // console.log(Authentication.user);
+    this.verifyAdmin();
   }
   setUserAs() {
     this.username = this.Authentication.user.email;
+  }
+  verifyAdmin() {
+    if (this.Authentication.user.role === 'admin') {
+      this.isAdmin = true;
+    }
   }
 }
 
