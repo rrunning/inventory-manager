b60465a382dc6582e041748fbc7d16831cf760b0
rrunning <ryanrunning@securitymetrics.com>
2018-01-23 19:08:50 -0700

    adds new product addition.

--- client/app/components/adminproducts/adminproducts.controller.js
+++ client/app/components/adminproducts/adminproducts.controller.js
@@ -1,8 +1,27 @@
 class AdminproductsController {
-	constructor() {
-
+	constructor($state, $stateParams, ProductsService, Admin) {
+		this.$state = $state;
+		this.Admin = Admin;
+		this.ProductsService = ProductsService;
+		this.addNew = false;
+		this.newProduct = {};
+		console.log('ProductsService:',this.ProductsService);
+	}
+	goToProduct(id) {
+		this.$state.go('productdetails', { id });
+	}
+	addNewProduct() {
+		this.addNew = true;
+	}
+	submitNewProduct() {
+		console.log(this.newProduct);
+		this.ProductsService.createProduct(this.newProduct).then(response => {
+			const createdProduct = this.newProduct;
+			this.newProduct = {};
+		});
 	}
 }
 
-AdminproductsController.$inject = [];
+
+AdminproductsController.$inject = ['$state', '$stateParams', 'ProductsService', 'Admin'];
 export default AdminproductsController;
