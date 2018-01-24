cc2f6f9ab30fa179a3f479a6b712ab0119f9bc3c
rrunning <ryanrunning@securitymetrics.com>
2018-01-23 20:20:08 -0700

    adds new transaction form on admin transactions page

--- client/app/components/adminTransactions/adminTransactions.controller.js
+++ client/app/components/adminTransactions/adminTransactions.controller.js
@@ -2,10 +2,14 @@ class AdmintransactionsController {
   constructor($state) {
     this.name = 'admintransactions';
     this.$state = $state;
+    this.addNew = false;
   }
   viewTransactionDetails(id) {
     // console.log(id)
     this.$state.go('transactionDetails', {id});
+  }
+  addNewTransaction() {
+    this.addNew = true;
   }
 }
 
